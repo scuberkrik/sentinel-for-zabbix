bc944552c5699ee3e54d82c124d4e84aae179e13
scuberkrik <107275266+scuberkrik@users.noreply.github.com>
2024-05-16 13:40:35 +0300

    Delete get_health_1.ps1

--- get_health_1.ps1
+++
@@ -1,5 +0,0 @@
-$FilePath = "C:\Users\admin\PycharmProjects\pythonProject\disk_1.json"
-$json = Get-Content $FilePath | ConvertFrom-Json
-$health = $json.Health -replace '%', ''
-[int]$intHealth = [int]$health
-$intHealth
